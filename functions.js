bdb7b32acc5913e80252df259a7eeb2117911acf
Artem <artemaz665@gmail.com>
2023-06-09 18:40:18 +0200

    finished second page!

---
+++ functions.js
@@ -0,0 +1,43 @@
+function changeText(card) {
+  var description = card.querySelector(".card1_text");
+  card.dataset.originalText = description.textContent;
+  description.textContent =
+    "The gradual information about atomic behaviour and more more more more...";
+}
+function resetText(card) {
+  var description = card.querySelector(".card1_text");
+  description.textContent = card.dataset.originalText;
+}
+
+function changeText2(card) {
+  var description = card.querySelector(".card2_text");
+  card.dataset.originalText = description.textContent;
+  description.textContent =
+    "The gradual information about atomic behaviour and more more more more...";
+}
+function resetText2(card) {
+  var description = card.querySelector(".card2_text");
+  description.textContent = card.dataset.originalText;
+}
+
+function changeText3(card) {
+  var description = card.querySelector(".card3_text");
+  card.dataset.originalText = description.textContent;
+  description.textContent =
+    "The gradual information about atomic behaviour and more more more more...";
+}
+function resetText3(card) {
+  var description = card.querySelector(".card3_text");
+  description.textContent = card.dataset.originalText;
+}
+
+function changeText4(card) {
+  var description = card.querySelector(".card4_text");
+  card.dataset.originalText = description.textContent;
+  description.textContent =
+    "The gradual information about atomic behaviour and more more more more...";
+}
+function resetText4(card) {
+  var description = card.querySelector(".card4_text");
+  description.textContent = card.dataset.originalText;
+}
